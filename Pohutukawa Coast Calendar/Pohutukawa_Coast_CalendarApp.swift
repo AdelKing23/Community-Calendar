8f34024770e24a8816cf2d4459b1c1f6fbd697da
AdelKing23 <isaacwellis@hotmail.com>
2026-06-01 00:41:36 +1200

    Initial Commit

---
+++ Pohutukawa Coast Calendar/Pohutukawa_Coast_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pohutukawa_Coast_CalendarApp.swift
+//  Pohutukawa Coast Calendar
+//
+//  Created by Isaac Wellis on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Pohutukawa_Coast_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
